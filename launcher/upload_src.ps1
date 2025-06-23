77d773a64898226ee145fcfea8e5bf8335d28d8b
Jos Verlinde <Jos_Verlinde@hotmail.com>
2025-06-23 15:00:10 +0200

    windows support: Add .ps1 script  for MicroPython ESP32 and update task details
    Signed-off-by: Jos Verlinde <Jos_Verlinde@hotmail.com>

---
+++ launcher/upload_src.ps1
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# copy the debugpy module into the lib folder
+mpremote resume mkdir lib 
+# mpremote cp -r python-ecosys/debugpy/debugpy :/lib
+
+# copy the src directory to the root of the ESP32 filesystem
+# workaround for windows issue with mpremote cp -r
+cd src 
+mpremote resume cp -r . :/
+cd ..
+
+# start the debugpy server on the ESP32
+mpremote run launcher/start_debugpy_esp32.py 
+
+# mpremote mount src + run launcher/start_debugpy_esp32.py 
